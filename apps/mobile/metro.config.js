9fb77495e47e9670d23de275c9c2fd4b4d95af2b
0xC1x <leonardoemn11@gmail.com>
2026-09-20 19:46:07 -0500

    feat(mobile): refresh app shell config and brand assets

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -9,4 +9,4 @@ config.resolver.unstable_enablePackageExports = true;
 // ponytail: withSentryConfig rompe `expo export --platform web` en @sentry/react-native 7.11
 // (determineDebugIdFromBundleSource recibe bundle undefined). Sentry sigue capturando
 // errores en web+nativo sin debugId; re-activar cuando Sentry publique fix para web.
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
